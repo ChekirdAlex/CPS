1627f2d3baad95502ffd98c5cfbfbfc35b725362
ChekirdAlex <alexeychekirda@gmail.com>
2022-10-17 09:39:42 +0300

    Added tablet and desktop versions

--- js/swiper.js
+++ js/swiper.js
@@ -1,31 +1,26 @@
 const slider = document.querySelector('.swiper');
-let init = false;  /*Slider status with resize. Default: off*/
+let init = false;
 let sliderBrands;
 
-function createSliderBrands (isActive) {
-    if (isActive) {
-        sliderBrands = new Swiper(slider, {
-            slidesPerView: 'auto',
-            spaceBetween: -40,
-            pagination: {
-                el: '.swiper-pagination',
-                type: 'bullets',
-            },
-        });
-    } else {
+function createSliderBrands () {
+    if (window.innerWidth < 768) {
+        if (!init) {
+            init = true;
+            sliderBrands = new Swiper(slider, {
+                slidesPerView: 'auto',
+                spaceBetween: 16,
+                pagination: {
+                    el: '.swiper-pagination',
+                    type: 'bullets',
+                    clickable: true,
+                },
+            });
+        }
+    } else if (init) {
         sliderBrands.destroy();
+        init = false;
     }
 }
 
-// resize
-window.addEventListener('resize', function () {
-    if (window.innerWidth < 768 && !init) {
-        init = true;
-        createSliderBrands(init);
-    } else if (window.innerWidth >= 768 && init) {
-        init = false;
-        createSliderBrands(init);
-    }
-})
-
-createSliderBrands(init);
+createSliderBrands();
+window.addEventListener('resize', createSliderBrands);
